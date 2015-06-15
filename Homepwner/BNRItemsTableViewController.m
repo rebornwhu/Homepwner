1487ea322ba1d37525bf29e6d3101925015a8c93
rebornwhu <rebornwhu@gmail.com>
2015-06-14 21:45:34 -0700

    Prep UITableView protocol

--- Homepwner/BNRItemsTableViewController.m
+++ Homepwner/BNRItemsTableViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "BNRItemsTableViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
 
 @interface BNRItemsTableViewController ()
 
@@ -17,6 +19,12 @@
 - (instancetype)init
 {
     self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
+    
     return self;
 }
 
