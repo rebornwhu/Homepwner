271b38e0955c2d302bd37ec4465f15a143ea3856
rebornwhu <rebornwhu@gmail.com>
2015-06-14 21:33:59 -0700

    Implement part of data store

---
+++ Homepwner/BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  Homepwner
+//
+//  Created by Xiao Lu on 6/14/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ Homepwner/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  Homepwner
+//
+//  Created by Xiao Lu on 6/14/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
