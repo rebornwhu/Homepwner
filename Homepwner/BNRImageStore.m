18acae048266de7354d60fa62ba4d5baeef67f9a
rebornwhu <rebornwhu@gmail.com>
2015-07-11 20:04:10 -0700

    Thread safe singleton image store

--- Homepwner/BNRImageStore.m
+++ Homepwner/BNRImageStore.m
@@ -18,8 +18,11 @@
 {
     static BNRImageStore *sharedStore = nil;
     
-    if (!sharedStore)
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
         sharedStore = [[self alloc] initPrivate];
+    });
+    
     return sharedStore;
 }
 
