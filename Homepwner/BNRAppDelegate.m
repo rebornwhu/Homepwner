4dc1669fb4d6086ba9dbff848076fb63de421796
rebornwhu <rebornwhu@gmail.com>
2015-07-14 20:48:01 -0700

    Add application lifecycle logging

--- Homepwner/BNRAppDelegate.m
+++ Homepwner/BNRAppDelegate.m
@@ -19,6 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     BNRItemsViewController *itemsViewController = [[BNRItemsViewController alloc] init];
@@ -37,6 +38,8 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -53,14 +56,17 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 @end
